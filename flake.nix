65fb8de721c795aca09fc0ddf065eac297deeb73
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-11-07 22:51:23 +0100

    From scratch.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "l";
+  
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    utils.url = github:numtide/flake-utils;
+  };
+  
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocaml
+            dune_3
+          ] ++ (with ocamlPackages; [
+            menhir
+            ocamlbuild
+            ocp-indent
+            findlib
+            merlin
+            utop
+          ]);
+        };
+    });
+}
